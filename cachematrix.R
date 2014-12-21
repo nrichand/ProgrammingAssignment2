d5a88f68d31ca44e6e8895a55e54882277118e3a
nrichand <nrichand@gmail.com>
2014-12-21 22:56:18 +0100

    Solve the exercice

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Theses functions allow to create a matrix which can store the 
+## last version of the inverse in order to avoid the cost of recalculate it every time
 
-## Write a short comment describing this function
-
+## Create a matrix, and prepare the cache to store the invert result
 makeCacheMatrix <- function(x = matrix()) {
+  x <<- y
+  inverse <<- NULL
 
+  get <- function() x
+  setinvert <- function(inv) inverse <<- inv
+  getinvert <- function() inverse
+    
+  list(get = get,
+       setinvert = setinvert,
+       getinvert = getinvert)
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinvert()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvert(m)
+  m 
 }
